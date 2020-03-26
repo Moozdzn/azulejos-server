e6f5debe1a941ebead03f4b66c339d13e5f4a28d
Diogo <moozdzn@gmail.com>
2020-03-26 17:15:07 +0000

    Add spatial nearSphere query

--- routes/azulejos.js
+++ routes/azulejos.js
@@ -7,21 +7,42 @@ require('dotenv/config');
 //var mongoDAO = require('../models/azulejosDAO');
 
 router.get('/', function(req, res, next) {
-    mongo.connect(process.env.DB_CONNECTION, function(err, client){
+    mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
 
         var db = client.db('app_azulejos');
         db.collection('azulejos_info').find({}).toArray(function(findErr, docs) {
             if(findErr) throw findErr;
-            console.log(docs);
+            console.log(JSON.stringify(docs));
             client.close();
-            res.send(docs)
+            res.send({docs})
           });
-    })
+    }) 
 });
 
-router.get('/teste', function(req,res,next){
-    res.send('OK')
+router.post('/', function(req,res,next){
+    mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
+        if(err) throw err;
+
+        var db = client.db('app_azulejos');
+        db.collection('azulejos_info').find({
+            "Localizacao":{
+                "$nearSphere":
+                {
+                    "$geometry": { 
+                        "type":"Point", 
+                        "coordinates": [req.body.lng,req.body.lat]},
+                    "$minDistance": 0,
+                    "$maxDistance": 10000
+                }
+            }
+        }).toArray(function(findErr, docs) {
+            if(findErr) throw findErr;
+            console.log(JSON.stringify(docs));
+            client.close();
+            res.send({docs})
+          });
+    })
 })
 
 module.exports = router;
