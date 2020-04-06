2b2f5dff1bd8df0e16fee202ff80a6658a913023
Diogo <moozdzn@gmail.com>
2020-04-06 19:21:38 +0100

    Changed API endpoints and structure

--- routes/sessoes.js
+++ routes/sessoes.js
@@ -5,22 +5,34 @@ var assert = require('assert');
 require('dotenv/config');
 var ObjectId = mongo.ObjectID;
 
-//var mongoDAO = require('../models/azulejosDAO');
-
-router.get('/', function(req, res, next) {
+// DEVOLVE NOME DOS AZULEJOS PARA AUTOCOMPLETE
+router.get('/:sessoes/azulejos/nome', function(req, res, next) {
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
-        if(err) throw err;
-
         var db = client.db('app_azulejos');
-        db.collection('azulejos_info').find({},{projection:{Nome:1}}).toArray(function(findErr, docs) {
-            if(findErr) throw findErr;
-            client.close();
-            res.send({docs})
-          });
+
+        if(err) throw err;
+        if(req.params.sessoes == "sessoes") {
+            db.collection('azulejos_info').find({},{projection:{Nome:1}}).toArray(function(findErr, docs) {
+                if(findErr) throw findErr;
+                client.close();
+                res.send({docs})
+              });
+        }
+        else{
+            var sessao = new ObjectId(req.params.sessoes);
+            db.collection('azulejos_info').find({"Sessao":{"$eq":sessao}},{projection:{Nome:1}}).toArray(function(findErr, docs) {
+                if(findErr) throw findErr;
+                client.close();
+                res.send({docs})
+              });
+        }
+
+        
+        
     }) 
 });
-
-router.post('/', function(req,res,next){
+//DEVOLVE LOCALIZACAO DOS AZULEJOS ATE 5KM DA POSICAO DO UTILIZADOR
+router.post('/sessoes/azulejos', function(req,res,next){
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
 
@@ -43,14 +55,28 @@ router.post('/', function(req,res,next){
           });
     })
 })
-
-router.get('/:id', function(req,res,next){
+//DEVOLVE INFORMACAO DE UM UNICO AZULEJO
+router.get('/sessoes/:id', function(req,res,next){
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
         var marker = new ObjectId(req.params.id)
         var db = client.db('app_azulejos');
         
         db.collection("azulejos_info").findOne({"_id":marker}, function(findErr, doc){
+            if(findErr) throw findErr;
+            client.close();
+            res.send(doc);
+        })          
+    })
+})
+//DEVOLVE INFORMACAO DA SESSAO
+router.get('/:id', function(req,res,next){
+    mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
+        if(err) throw err;
+        var marker = new ObjectId(req.params.id)
+        var db = client.db('app_azulejos');
+        
+        db.collection("azulejos_sessoes").findOne({"_id":marker}, function(findErr, doc){
             if(findErr) throw findErr;
             client.close();
             res.send(doc);
