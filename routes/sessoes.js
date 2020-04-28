868efa044b24e4fbdc9b22199cf1a1d914ce78e7
Diogo <moozdzn@gmail.com>
2020-04-28 19:26:14 +0100

    GDrive module-Changed API endpoints Store and retrieve img -> GDrive

--- routes/sessoes.js
+++ routes/sessoes.js
@@ -5,6 +5,17 @@ var assert = require('assert');
 require('dotenv/config');
 var ObjectId = mongo.ObjectID;
 
+
+
+
+var path = require('path');
+var fs = require('fs');
+//
+const {google} = require('googleapis');
+
+var outDir = path.join(__dirname,"uploads");
+
+
 // DEVOLVE NOME DOS AZULEJOS PARA AUTOCOMPLETE
 router.get('/:sessoes/azulejos/nome', function(req, res, next) {
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
@@ -32,10 +43,9 @@ router.get('/:sessoes/azulejos/nome', function(req, res, next) {
     }) 
 });
 //DEVOLVE LOCALIZACAO DOS AZULEJOS ATE 5KM DA POSICAO DO UTILIZADOR
-router.post('/sessoes/azulejos', function(req,res,next){
+router.get('/sessoes/azulejos', function(req,res,next){
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
-
         var db = client.db('app_azulejos');
         db.collection('azulejos_info').find({
             "Localizacao":{
@@ -43,7 +53,7 @@ router.post('/sessoes/azulejos', function(req,res,next){
                 {
                     "$geometry": { 
                         "type":"Point", 
-                        "coordinates": [req.body.lng,req.body.lat]},
+                        "coordinates": [parseFloat(req.query.lng),parseFloat(req.query.lat)]},
                     "$minDistance": 0,
                     "$maxDistance": 5000
                 }
@@ -57,6 +67,7 @@ router.post('/sessoes/azulejos', function(req,res,next){
 })
 //DEVOLVE INFORMACAO DE UM UNICO AZULEJO
 router.get('/sessoes/:id', function(req,res,next){
+
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
         var marker = new ObjectId(req.params.id)
@@ -65,12 +76,44 @@ router.get('/sessoes/:id', function(req,res,next){
         db.collection("azulejos_info").findOne({"_id":marker}, function(findErr, doc){
             if(findErr) throw findErr;
             client.close();
-            res.send(doc);
+            require("../gdrive/gdrive-auth")((auth) => {
+                const drive = google.drive({version: 'v3', auth});
+                var pageToken = null;
+                drive.files.list({
+                    q: "mimeType='application/vnd.google-apps.folder' and name contains '"+req.params.id+"'",
+                    fields: 'nextPageToken, files(id)',
+                    spaces: 'drive',
+                    pageToken: pageToken
+                }).then((response)=>{
+                    console.log(response.data.files[0].id)
+                    drive.files.list({
+                        q: `'${response.data.files[0].id}' in parents`,
+                        fields: 'nextPageToken, files(id, name)',
+                        spaces: 'drive',
+                        pageToken: pageToken
+                      }).then((response)=> {
+                            var filePath = './temporary_uploads/'+response.data.files[0].name
+                            var dest = fs.createWriteStream(filePath);
+                            drive.files.get({fileId: response.data.files[0].id,alt: 'media'},{responseType: 'stream'}).then((response)=>{
+                                response.data.on('end', function () {
+                                    var bitmap = fs.readFileSync(filePath, { encoding: 'base64' });
+                                    console.log('Done');
+                                    doc['file'] = bitmap;
+                                    res.send(doc);
+                                    }).on('error', function (err) {
+                                    console.log('Error during download', err);
+                                    }).pipe(dest);
+                            })
+                      });
+                });
+                
+            });
         })          
     })
 })
 //DEVOLVE INFORMACAO DA SESSAO
 router.get('/:id', function(req,res,next){
+    
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
         var marker = new ObjectId(req.params.id)
@@ -79,7 +122,73 @@ router.get('/:id', function(req,res,next){
         db.collection("azulejos_sessoes").findOne({"_id":marker}, function(findErr, doc){
             if(findErr) throw findErr;
             client.close();
+
             res.send(doc);
+        })          
+    })
+    
+})
+router.post('/sessoes/azulejos', function(req,res,next){   
+    const imageBuffer = new Buffer(req.body.file, "base64");
+    const filePath = "./temporary_uploads/"+req.body.nome+".jpg";
+    fs.writeFileSync(filePath, imageBuffer);
+    var document = {
+        "Nome": req.body.nome,
+        "Ano": req.body.ano,
+        "Info": req.body.info,
+        "Condicao": req.body.condicao,
+        "Localizacao": {
+            "type":"Point",
+            "coordinates": req.body.coordinates
+        }
+    }
+    mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
+        if(err) throw err;
+        var db = client.db('app_azulejos');
+        
+        db.collection("azulejos_info").insertOne(document, function(findErr, doc){
+            if(findErr) throw findErr;
+            client.close();
+            require("../gdrive/gdrive-auth")((auth) => {
+                const drive = google.drive({version: 'v3', auth});
+                var fileMetadata = {
+                    'name': doc.insertedId,
+                    'mimeType': 'application/vnd.google-apps.folder'
+                  };
+                  drive.files.create({
+                    resource: fileMetadata,
+                    fields: 'id'
+                  }, function (err, file) {
+                    if (err) {
+                      // Handle error
+                      console.error(err);
+                      res.status(500).send('Aconteceu um erro interno ao guardar a fotografia. Tente novamente mais tarde.');
+                    } else {
+                        var folderId = file.data.id;
+                        var fileMetadata = {
+                            'name': 'file.jpg',
+                            parents: [folderId]
+                        };
+                        var media = {
+                            mimeType: 'image/jpeg',
+                            body: fs.createReadStream("./temporary_uploads/"+req.body.nome+".jpg")
+                        };
+                        drive.files.create({
+                            resource: fileMetadata,
+                            media: media,
+                            fields: 'id'
+                        }, function (err, file) {
+                            if (err) {
+                                // Handle error
+                                console.error(err);
+                                res.status(500).send('Aconteceu um erro interno ao guardar a fotografia. Tente novamente mais tarde.');
+                            } else {
+                                res.status(200).send('Submissão recebida');
+                            }
+                        });
+                    }
+                  });
+            });
         })          
     })
 })
