66c2aba815f9fff65fbf3f61b57cb69d50773071
Diogo <moozdzn@gmail.com>
2020-05-15 21:50:51 +0100

    Changed api to allow for more than one submission

--- routes/sessoes.js
+++ routes/sessoes.js
@@ -7,6 +7,11 @@ var ObjectId = mongo.ObjectID;
 var fs = require('fs');
 const https = require('https');
 
+/*
+/api/sessoes/azulejos/nome
+/api/sessoes/azulejos
+*/
+
 // DEVOLVE NOME DOS AZULEJOS PARA AUTOCOMPLETE
 router.get('/:sessoes/azulejos/nome', function(req, res, next) {
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
@@ -25,7 +30,7 @@ router.get('/:sessoes/azulejos/nome', function(req, res, next) {
             db.collection('azulejos_info').find({"Sessao":{"$eq":sessao}},{projection:{Nome:1}}).toArray(function(findErr, docs) {
                 if(findErr) throw findErr;
                 client.close();
-                res.send({docs})
+                res.send({docs});
               });
         }  
     }) 
@@ -58,14 +63,35 @@ router.get('/sessoes/:id', function(req,res,next){
 
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
-        var marker = new ObjectId(req.params.id)
-        var db = client.db('app_azulejos');
+        console.log(req.params.id)
+        try{
+            var marker = new ObjectId(req.params.id);
+            var db = client.db('app_azulejos');
         
         db.collection("azulejos_info").findOne({"_id":marker}, function(findErr, doc){
             if(findErr) throw findErr;
             client.close();
-            res.send(doc)
-        })          
+            https.get('https://storage.bunnycdn.com/azulejos/5eb98addcf6b403f84fe0c09/?AccessKey='+process.env.ACCESS_KEY, (response) => {
+        
+            console.log('statusCode:', response.statusCode);
+      
+            response.on('data', (d) => {
+                var json = JSON.parse(d.toString()).length
+                doc['nrImages'] = json
+                res.send(doc);
+            });
+      
+            }).on('error', (e) => {
+                console.error(e);
+            });
+        })  
+        
+        }
+        catch(e){
+            console.log("Discard");
+            res.sendStatus(404);
+        }
+                
     })
 })
 //DEVOLVE INFORMACAO DA SESSAO
@@ -79,17 +105,22 @@ router.get('/:id', function(req,res,next){
         db.collection("azulejos_sessoes").findOne({"_id":marker}, function(findErr, doc){
             if(findErr) throw findErr;
             client.close();
-
             res.send(doc);
         })          
     })
     
 })
-router.post('/sessoes/azulejos', function(req,res,next){   
-    const imageBuffer = new Buffer(req.body.file, "base64");
-    const filePath = "./temporary_uploads/1.jpg";
-    fs.writeFileSync(filePath, imageBuffer); 
-
+// INSERE UM AZULEJO NA BASE DE DADOS E AS RESPETIVAS IMAGENS NO BUNNYCDN
+router.post('/sessoes/azulejos', function(req,res,next){
+    var filePathArray = [];
+    var teste = []
+    var body;
+    for (var i in req.body.file){
+        const imageBuffer = new Buffer(req.body.file[i], "base64");
+        const filePath = "./temporary_uploads/"+i+".jpg";
+        filePathArray.push(filePath);
+        fs.writeFileSync(filePath, imageBuffer);
+    }
     var document = {
         "Nome": req.body.nome,
         "Ano": req.body.ano,
@@ -99,7 +130,12 @@ router.post('/sessoes/azulejos', function(req,res,next){
             "type":"Point",
             "coordinates": req.body.coordinates
         }
+    }   
+    if(req.body.sessionID != false){
+        var sessionID = new ObjectId(req.body.sessionID);
+        document['Sessao'] = sessionID
     }
+    
     mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
         if(err) throw err;
         var db = client.db('app_azulejos');
@@ -107,42 +143,126 @@ router.post('/sessoes/azulejos', function(req,res,next){
         db.collection("azulejos_info").insertOne(document, function(findErr, doc){
             if(findErr) throw findErr;
             client.close();
-            console.log(doc)
-            fs.readFile(filePath, function(err, data) {
-                if (err) throw err;
-              
-                var options = {
-                    'method': 'PUT',
-                    'hostname': 'storage.bunnycdn.com',
-                    'path': '/azulejos/'+doc.insertedId+'/1.jpg?AccessKey='+process.env.ACCESS_KEY,
-                    'headers': {
-                      'Content-Type': 'image/jpeg'
-                    } 
-                  };
-                var reqBunny = https.request(options, function (resBunny) {
-                    var chunks = [];
-                  
-                    resBunny.on("data", function (chunk) { chunks.push(chunk); });
-                  
-                    resBunny.on("end", function (chunk) {
-                      var body = Buffer.concat(chunks);
-                      res.send(body.toString());
+            for(const i in filePathArray){
+                console.log(filePathArray[i])
 
-                    });
-        
-                    resBunny.on("error", function (error) { res.send(error); });
-                  });
-                reqBunny.write(data);
-                reqBunny.end();
-              });
+                fs.readFile(filePathArray[i], function(err, data) {
+                    if (err) throw err;
+                    console.log(filePathArray[i])
+                    var options = {
+                        'method': 'PUT',
+                        'hostname': 'storage.bunnycdn.com',
+                        'path': '/azulejos/'+doc.insertedId+'/'+i+'.jpg?AccessKey='+process.env.ACCESS_KEY,
+                        'headers': {
+                          'Content-Type': 'image/jpeg'
+                        } 
+                      };
+                    var reqBunny = https.request(options, function (resBunny) {
+                        var chunks = [];
+    
+                        resBunny.on("data", function (chunk) { chunks.push(chunk); });
+                        resBunny.on("end", function (chunk) {
+                          body = Buffer.concat(chunks);
+                          teste.push(body.toString());
+                          
+                        });
+                        resBunny.on("error", function (error) { res.send(error); });
+                      });
+                    reqBunny.write(data);
+                    reqBunny.end();
+                });
+            }
+            res.send('done'); 
         })          
     })
 })
 
 
 
-router.put('/teste',function(requestes,response,next){
-     
+router.post('/sessoes',function(req,res,next){
+    console.log(req.body.azulejos[1].Files.length);
+
+    uploadPhotos(req.body.azulejos);
+    mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
+        if(err) throw err;
+        var sessionID = new ObjectId(req.body.sessao._id);
+        var userID = new ObjectId(req.body.sessao.idAutor);
+        var db = client.db('app_azulejos');
+        
+        db.collection("azulejos_sessoes").insertOne({"_id":sessionID,"estado":req.body.sessao.estado, "info":req.body.sessao.info,"idAutor":userID,"azulejos":req.body.sessao.azulejos}, function(findErr, doc){
+            if(findErr) throw findErr;
+            var documents = [];
+            for(var i in req.body.azulejos){
+                var azulejo = req.body.azulejos[i];
+                var document = {
+                    "_id": new ObjectId(azulejo._id),
+                    "Nome": azulejo.Nome,
+                    "Ano": azulejo.Ano,
+                    "Info": azulejo.Info,
+                    "Condicao": azulejo.Condicao,
+                    "Sessao" : sessionID,
+                    "Localizacao": {
+                        "type":"Point",
+                        "coordinates": azulejo.Localizacao
+                    }
+                }
+                documents.push(document);
+            }
+            console.log(documents)
+            db.collection("azulejos_info").insertMany(documents, function(findErr, doc){
+                if(findErr) throw findErr;
+                console.log(doc)
+                client.close();
+            }); 
+        })  
+        res.send("done")        
+    })
 })
 
+function uploadPhotos(azulejos){
+    
+    var teste = []
+    var body;
+    for(const j in azulejos){
+        var filePathArray = [];
+        for (var i in azulejos[j].Files){
+            
+            const imageBuffer = new Buffer(azulejos[j].Files[i], "base64");
+            const filePath = "./temporary_uploads/"+azulejos[j]._id+"-"+i+".jpg";
+            filePathArray.push(filePath);
+            fs.writeFileSync(filePath, imageBuffer);
+        }   
+                for(const i in filePathArray){
+                    console.log(filePathArray[i])
+    
+                    fs.readFile(filePathArray[i], function(err, data) {
+                        if (err) throw err;
+                        console.log(filePathArray[i])
+                        var options = {
+                            'method': 'PUT',
+                            'hostname': 'storage.bunnycdn.com',
+                            'path': '/azulejos/'+azulejos[j]._id+'/'+i+'.jpg?AccessKey='+process.env.ACCESS_KEY,
+                            'headers': {
+                              'Content-Type': 'image/jpeg'
+                            } 
+                          };
+                        var reqBunny = https.request(options, function (resBunny) {
+                            var chunks = [];
+        
+                            resBunny.on("data", function (chunk) { chunks.push(chunk); });
+                            resBunny.on("end", function (chunk) {
+                              body = Buffer.concat(chunks);
+                              teste.push(body.toString());
+                              
+                            });
+                            resBunny.on("error", function (error) { res.send(error); });
+                          });
+                        reqBunny.write(data);
+                        reqBunny.end();
+                    });
+                }
+    }
+    
+
+}
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
+var ObjectId = mongo.ObjectID;
 
 require('dotenv/config');
+
 
 /* GET users listing. */
 router.post('/', function(req,res,next){
@@ -20,6 +22,20 @@ router.post('/', function(req,res,next){
           else{res.send('Internal error')}
       })
   })
+})
+
+router.get('/:id/sessoes', function(req,res,next){
+  var userID = new ObjectId(req.params.id);
+  mongo.connect(process.env.DB_CONNECTION,{ useUnifiedTopology: true }, function(err, client){
+    if(err) throw err;
+    var db = client.db('app_azulejos');
+    
+    db.collection('azulejos_sessoes').find({"idAutor": userID}).toArray(function(findErr, docs) {
+      if(findErr) throw findErr;
+      client.close();
+      res.send({docs})
+    });
+})
 })
 
 module.exports = router;
